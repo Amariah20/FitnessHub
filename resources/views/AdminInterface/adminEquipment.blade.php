f4abbe5f40e909d926b710ae8ae9250cbd74cea8
Amariah20 <rigodonamariah16@gmail.com>
2023-11-28 08:57:31 +0000

    equipment admin

---
+++ resources/views/AdminInterface/adminEquipment.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.adminPage')
+
+@section('content')
+
+
+
+<h2>Offerings</h2>
+@if (session('Success'))
+    <h6 class="alert alert-success">{{ session('Success') }}</h6>
+@endif
+
+<a href="{{route('AdminAddEquipment', ['Gym_id' => $Gym_id])}}"><button>Add New Equipment</button></a>
+
+
+
+
+<table class="table table-striped">
+  <thead class="thead-light">
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Decription</th>
+      <th scope="col">Edit</th>
+      
+    </tr>
+  </thead>
+
+  <tbody>
+  @foreach($equipments as $equipment) 
+  
+    <tr>
+      
+      <td>{{$equipment->name}}</td>
+      <td>{{$equipment->description}}</td>
+     
+      
+      <td><a href="{{route('EditEquipment', ['Equipment_id' => $equipment->equipment_id] )}}"><button type= "submit">Edit</button></a></td>
+      <td><a href="{{route('DeleteEquipment',['Equipment_id' => $equipment->equipment_id])}}"><button type= "submit">Delete</button></a></td>
+    </tr>
+ @endforeach
+ </tbody>
+ </table>
+      
+      
+    
+
+
+
+
+
+
+@endsection
---
+++ resources/views/AdminInterface/editEquipment.blade.php
@@ -0,0 +1,25 @@
+<div class="container">
+<div class="card-header">Edit Offering</div>
+<form method="POST" action="{{route('UpdateEquipment',  ['Equipment_id' => $equipment->equipment_id])}}">
+@csrf
+@method("patch")
+
+    <div class="mb">
+        <label class="label">Name</label>
+        <input type="text" name="name" class="form-control" value="{{$equipment->name}}" required>
+    </div>
+  
+   
+    <div class="mb">
+        <label class="label">Write a brief description</label>
+        <input class="form-control" name="description"   value="{{$equipment->description}}" required></input>
+    </div>
+
+
+</div>
+
+ 
+        <button type="submit">Update </button>
+
+</form>
+<div>
