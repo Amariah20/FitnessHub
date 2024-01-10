0feb2b9781707330d4ff2ee6a6f61f66c687ebd9
Amariah20 <rigodonamariah16@gmail.com>
2024-01-10 12:13:56 +0000

    validation works. routing no

---
+++ app/Http/Requests/ClassValidation.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+//used this for guidance: https://laravel.com/docs/10.x/validation  
+class ClassValidation extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'=> 'required|string|max:255',
+            'location'=>'required|string|max:255',
+            'description'=>'required|string|max:255',
+            'schedule'=> 'required|string|max:255',
+            'capacity'=>'required|numeric',
+            'duration'=>'required|numeric',
+            'price'=>'required|numeric',
+
+        ];
+    }
+}
