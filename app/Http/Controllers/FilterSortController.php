239784e104ddc09dedc53fac4667f0fb9d3205d8
Amariah20 <rigodonamariah16@gmail.com>
2024-01-12 10:07:18 +0000

    sort by price works

---
+++ app/Http/Controllers/FilterSortController.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Membership;
+use App\Models\Gym;
+
+class FilterSortController extends Controller
+{
+    public function sortMembershipPrice(Request $req){
+        $sort=$req->sort;
+        if ($sort=="monthly-low"){
+           
+            $memberships=Membership::where('membership_type','monthly')->get();
+            $sortedMemberships= $memberships->sortBy('price'); //sortBy sorts in ascending order
+            //dd($sortedMemberships);
+
+            $gym_ids=$sortedMemberships->pluck('gym_id')->toArray(); //copied from searchController
+           // dd($gym_ids);
+            $gyms=Gym::whereIn('Gym_id',$gym_ids)->get();
+           // dd($gyms);
+            // $gym_ids=$memberships->pluck('gym_id')->toArray();
+           // $gyms= Gym::whereIn('Gym_id', $gym_ids)->orderBy($memberships->price, 'asc')->get();
+           // ->orderByRaw("FIELD(Gym_id),".implode(',',$gym_ids).")")
+            //->get(); 
+             
+            $sortedGyms=collect([]); //creating a collection of ordered gyms based on the order of memberships
+
+            /**go through sorted memberships as it stores the memberships in the proper order. 
+             * go through gyms has it has the gyms associated with the memberships.
+             * find the gym whose id matches the gym id of the first membership in the sorted list
+             * check that the gym actually exists, and add it to the collection of sorted gyms
+             * repeat the process. now it will go to the next gym in the ordered membership list
+             * */ 
+            foreach($sortedMemberships as $sortedMembership){
+                $gym=$gyms->where('Gym_id', $sortedMembership->gym_id)->first();
+                if($gym){
+                    $sortedGyms->push($gym);
+                }
+            }
+            $gyms=$sortedGyms;
+
+            return view ("/gymAll",compact('gyms'));
+
+
+        } else if ($sort=="monthly-high"){
+
+            $memberships=Membership::where('membership_type','monthly')->get();
+
+            $sortedMemberships= $memberships->sortByDesc('price'); //sorts in descending order
+
+            $gym_ids=$sortedMemberships->pluck('gym_id')->toArray(); 
+
+            $gyms=Gym::whereIn('Gym_id',$gym_ids)->get();
+
+            $sortedGyms=collect([]);
+
+            foreach($sortedMemberships as $sortedMembership){
+                $gym=$gyms->where('Gym_id', $sortedMembership->gym_id)->first();
+                if($gym){
+                    $sortedGyms->push($gym);
+                }
+            }
+
+            $gyms=$sortedGyms;
+
+            return view ("/gymAll",compact('gyms'));
+
+       
+    } else if($sort=="annual-low"){
+        $memberships=Membership::where('membership_type','annual')->get();
+        $sortedMemberships= $memberships->sortBy('price');
+
+        $gym_ids=$sortedMemberships->pluck('gym_id')->toArray(); 
+        $gyms=Gym::whereIn('Gym_id',$gym_ids)->get();
+
+        $sortedGyms=collect([]);
+
+        foreach($sortedMemberships as $sortedMembership){
+            $gym=$gyms->where('Gym_id', $sortedMembership->gym_id)->first();
+            if($gym){
+                $sortedGyms->push($gym);
+            }
+        }
+        $gyms=$sortedGyms;
+
+        return view ("/gymAll",compact('gyms'));
+
+    }else if($sort=="annual-high"){
+        $memberships=Membership::where('membership_type','annual')->get();
+
+        $sortedMemberships= $memberships->sortByDesc('price'); //sorts in descending order
+
+        $gym_ids=$sortedMemberships->pluck('gym_id')->toArray(); 
+
+        $gyms=Gym::whereIn('Gym_id',$gym_ids)->get();
+
+        $sortedGyms=collect([]);
+
+        foreach($sortedMemberships as $sortedMembership){
+            $gym=$gyms->where('Gym_id', $sortedMembership->gym_id)->first();
+            if($gym){
+                $sortedGyms->push($gym);
+            }
+        }
+
+        $gyms=$sortedGyms;
+
+        return view ("/gymAll",compact('gyms'));
+
+    } else if($sort=="daily-low"){
+        $memberships=Membership::where('membership_type','daily')->get();
+        $sortedMemberships= $memberships->sortBy('price');
+
+        $gym_ids=$sortedMemberships->pluck('gym_id')->toArray(); 
+        $gyms=Gym::whereIn('Gym_id',$gym_ids)->get();
+
+        $sortedGyms=collect([]);
+
+        foreach($sortedMemberships as $sortedMembership){
+            $gym=$gyms->where('Gym_id', $sortedMembership->gym_id)->first();
+            if($gym){
+                $sortedGyms->push($gym);
+            }
+        }
+        $gyms=$sortedGyms;
+
+        return view ("/gymAll",compact('gyms'));
+        
+
+    }else if($sort=="daily-high"){
+        $memberships=Membership::where('membership_type','daily')->get();
+        $sortedMemberships= $memberships->sortByDesc('price');
+
+        $gym_ids=$sortedMemberships->pluck('gym_id')->toArray(); 
+        $gyms=Gym::whereIn('Gym_id',$gym_ids)->get();
+
+        $sortedGyms=collect([]);
+
+        foreach($sortedMemberships as $sortedMembership){
+            $gym=$gyms->where('Gym_id', $sortedMembership->gym_id)->first();
+            if($gym){
+                $sortedGyms->push($gym);
+            }
+        }
+        $gyms=$sortedGyms;
+
+        return view ("/gymAll",compact('gyms'));
+    }
+        
+
+        
+    }
+}
