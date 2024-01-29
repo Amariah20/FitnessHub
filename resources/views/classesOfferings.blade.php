c54e38dbbcd88b1db0ad9693ef2966d34e1eb770
Amariah20 <rigodonamariah16@gmail.com>
2024-01-29 20:32:23 +0000

    design

--- resources/views/classesOfferings.blade.php
+++ resources/views/classesOfferings.blade.php
@@ -1,19 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Classes and Offerings</h1>
-<h2>Classes:</h2>
-    @foreach($classes as $class)
-       <a href= "{{route('classShow', ['Class_id'=>$class->Class_id])}}"> <p>{{ $class->name }}</p></a>
-       
-        <hr>
-    @endforeach
 
-    <h2>Offerings:</h2>
-    @foreach($offerings as $offering)
-    <a href= "{{route('offeringShow', ['Offering_id'=>$offering->offerings_id])}}">   <p>{{ $offering->name }}</p></a>
-        
-        <hr>
-    @endforeach
-
+<div class="classes_offerings">
+    <h1>Classes and Offerings</h1>
+    <div class="class_list">   
+        <h2>Classes</h2>
+            @foreach($classes as $class)
+            <a href= "{{route('classShow', ['Class_id'=>$class->Class_id])}}"> <p>{{ $class->name }}</p></a>
+            
+                <hr class="review_line">
+            @endforeach
+    </div>
+    <div class="offerings">
+        <h2>Offerings:</h2>
+        @foreach($offerings as $offering)
+        <a href= "{{route('offeringShow', ['Offering_id'=>$offering->offerings_id])}}">   <p>{{ $offering->name }}</p></a>
+            
+        <hr class="review_line">
+        @endforeach
+    </div>
+</div>
 @endsection
