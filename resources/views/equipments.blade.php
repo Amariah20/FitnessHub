5f1de363410535bed5b0a1e6d721e1717977168e
Amariah20 <rigodonamariah16@gmail.com>
2024-01-31 11:45:58 +0000

    equipment page

--- resources/views/equipments.blade.php
+++ resources/views/equipments.blade.php
@@ -1,11 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="equipments">
+<h1>Equipments</h1>
+
+<div class="row row-cols-1 row-cols-md-3 g-4">
+  
+
 @foreach($equipments as $equipment)
- <li>{{$equipment->name}}</li>
- <li>{{$equipment->description}}</li>
- <hr>
+<div class="col">
+    <div class="card border-secondary mb-3" style="width: 75%;">
+            <div class="card-header">
+            {{$equipment->name}}
+            </div>
+        <div class="card-body">
+                <h6 class="card-subtitle mb-2 text-muted">Equipment</h6>
+                <p class="card-text">{{$equipment->description}}</p>
+
+    </div>
+</div>
+</div>
 
 @endforeach
+</div>
 
 @endsection
