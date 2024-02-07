e872a8aefb9da635a8ab1c59c1dc0b25cb08ad84
Amariah20 <rigodonamariah16@gmail.com>
2024-02-07 10:58:13 +0000

    about us page

--- resources/views/AboutUs.blade.php
+++ resources/views/AboutUs.blade.php
@@ -2,21 +2,23 @@
 
 @section('content')
 
-<img src="{{asset('images/aboutUs.png')}}" style="width:100%; height:300px;">
+
+<img src="{{asset('images/aboutUs.jpg')}}" style="width: 100%; height:750px;" >
+
 <div class="about-us">
 <h1>About Us</h1>
 
 <h2>Fitness Hub: Bridging the Gap to a Healthier You</h2>
 
-<p>At Fitness Hub, we ae revolutionising the way fitness centres are discovered. Our platform makes it easier to find fitness centres.</p>
+<p>At Fitness Hub, we are revolutionising the way fitness centres are discovered. Our platform makes it easier to find fitness centres.</p>
 
 <p>We believe that health and well-being are fundamental human rights, in line with the UN sustainable Development Goal of good health and well-being.</p>
 
 <p>By connecting you with fitness centres in Seychelles, we are making the path to a healthier lifestyle easier to navigate.</p>
 
-<i>Join us in promoting a fitter and healthier Seychelles!</i>
+<p class="below">Join us in promoting a fitter and healthier Seychelles!</p>
 
-<i>Discover gyms and fitness centres, right here on Fitness Hub</i>
+<p class="below">Discover gyms and fitness centres, right here on Fitness Hub</p>
 </div>
 
 @endSection
