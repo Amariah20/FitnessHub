e84e972af3dd8ae610612fc24bcedfd67fbf7ba2
Amariah20 <rigodonamariah16@gmail.com>
2024-02-27 20:47:36 +0000

    google.marker

--- public/maps.js
+++ public/maps.js
@@ -55,7 +55,8 @@ fetch('/locations')
             });
 
             function createMarker(latlng, icon, title){
-                var marker = new google.maps.Marker({
+              //var marker = new google.maps.Marker({
+                var marker = new google.maps.marker({
                 position: latlng, 
                 map: map,
                 icon:icon,
