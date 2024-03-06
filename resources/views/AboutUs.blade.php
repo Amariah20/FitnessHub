d386b3c08e002387759e739948df873db138736a
Amariah20 <rigodonamariah16@gmail.com>
2024-03-06 07:29:49 +0000

    about us

--- resources/views/AboutUs.blade.php
+++ resources/views/AboutUs.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 
-<img src="{{asset('images/AboutUs3.jpg')}}" style="width: 100%; height:750px;" >
+<img src="{{asset('images/FitnessHubLogo.png')}}" style="width: 100%; height:750px;" >
 
 <div class="about-us">
 <h1>About Us</h1>
