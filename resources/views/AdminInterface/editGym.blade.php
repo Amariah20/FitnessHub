c1e62ae64de4c596adb8b8febabfb9113582bead
Amariah20 <rigodonamariah16@gmail.com>
2024-03-12 13:41:07 +0000

    style

--- resources/views/AdminInterface/editGym.blade.php
+++ resources/views/AdminInterface/editGym.blade.php
@@ -136,7 +136,7 @@
         </div>
 
 
-        <div class="card" style="width: 18rem; margin-top:-750px;padding-bottom:10px;">
+        <div class="card" style="width: 18rem; margin-top:-650px;padding-bottom:10px;">
             <div class="icon">
                 <svg  xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"  class="bi bi-info-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
