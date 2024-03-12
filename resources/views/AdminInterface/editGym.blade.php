cf158654a9a590faefe3a7bd05d6afde6dee9a39
Amariah20 <rigodonamariah16@gmail.com>
2024-03-12 15:42:35 +0000

    added icons. prev was fine

--- resources/views/AdminInterface/editGym.blade.php
+++ resources/views/AdminInterface/editGym.blade.php
@@ -122,7 +122,7 @@
   
 
 
-<div class="card" style="width: 18rem; margin-top:-800px;">
+<div class="card" style="width: 18rem; margin-top:-740px;">
             <div class="icon">
                 <svg  xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"  class="bi bi-info-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
@@ -133,6 +133,23 @@
                         
                         <p class="card-text">Please specifiy your opening hours in detail. If you have different opening hours for different days, please separate them with a dot (.) </p>
                         <p class="card-text">   For example: Monday to Friday 6am to 7pm. Saturday 9am to 12pm.</p>
+                        <br>
+                        <div class="icon">
+                          <svg  xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"  class="bi bi-info-circle" viewBox="0 0 16 16">
+                          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                          <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+                          </svg>
+                        </div>
+
+                        <p class="card-text">Please enter a squared image (with equal width and height) for the logo. Please ensure that the banner is at least 1920x1080 pixels.</p>
+
+                        <br>
+                        <div class="icon">
+                          <svg  xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"  class="bi bi-info-circle" viewBox="0 0 16 16">
+                          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                          <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
+                          </svg>
+                        </div>
 
                           <!--the below instructions are from google maps documentation: https://support.google.com/maps/answer/18539?hl=en-GB&co=GENIE.Platform%3DDesktop  -->
                           <p class="card-text">In order to display your business on the map, please enter your latitude and longitude. This step is optional. 
@@ -153,7 +170,7 @@
                                             <li>Correct: 41.40338, 2.17403 </p>
                                              <li>Incorrect: 41,40338, 2,17403 </p></p>
 
-                                             <p class="card-text">Please enter a squared image (with equal width and height) for the logo. Please ensure that the banner is at least 1920x1080 pixels.</p>
+                                            
                         
                     </div>
         </div>
