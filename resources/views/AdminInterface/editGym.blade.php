1cfdecac76529c2de784c1dfa8bbe8c7b4de99d4
Amariah20 <rigodonamariah16@gmail.com>
2024-03-12 11:14:52 +0000

    fixing

--- resources/views/AdminInterface/editGym.blade.php
+++ resources/views/AdminInterface/editGym.blade.php
@@ -143,8 +143,7 @@
                         
                     </div>
         </div>
-
-        <div class="card" style="width: 35rem; margin-top:19px;">
+        <div class="card" style="width: 18rem; margin-top:280px;">
             <div class="icon">
                 <svg  xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"  class="bi bi-info-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
@@ -152,11 +151,9 @@
                 </svg>
             </div>
                     <div class="card-body">
-                        <!--the below instructions are from google maps documentation: https://support.google.com/maps/answer/18539?hl=en-GB&co=GENIE.Platform%3DDesktop  -->
-                        <p class="card-text">In order to display your business on the map, please enter your latitude and longitude. This step is optional. 
-                            <p class="card-text"> To get the coordinates for your business: </p>
-
-                            <ul style=" font-size:20px;">
+                        
+                        <p class="card-text"> To get the coordinates for your business:</p>
+                        <ul style=" font-size:20px;">
                                     <li> Open <a href= "https://www.google.com/maps" target="_blank" style="color:blue; text-decoration:underline;">Google Maps</a>.  </li>
 
                                     <li> Right-click the place or area on the map. </li>
@@ -170,9 +167,9 @@
                                         <ul style=" font-size:20px;">
                                             <li>Correct: 41.40338, 2.17403 </li> 
                                              <li>Incorrect: 41,40338, 2,17403 </li> </ul></p>
-                        
                     </div>
         </div>
+        
 
 
 
