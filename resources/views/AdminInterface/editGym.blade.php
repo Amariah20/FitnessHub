3e7c1f8d30926bc412fc73c3582d90a898c0dd69
Amariah20 <rigodonamariah16@gmail.com>
2024-03-12 16:05:30 +0000

    fixing info

--- resources/views/AdminInterface/editGym.blade.php
+++ resources/views/AdminInterface/editGym.blade.php
@@ -129,7 +129,7 @@
                 <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0"/>
                 </svg>
             </div>
-                    <div class="card-body">
+                    <div class="card-body" style="font-size:20px;">
                         
                         <p class="card-text">Please specifiy your opening hours in detail. If you have different opening hours for different days, please separate them with a dot (.) </p>
                         <p class="card-text">   For example: Monday to Friday 6am to 7pm. Saturday 9am to 12pm.</p>
@@ -143,7 +143,7 @@
 
                         <p class="card-text">Please enter a squared image (with equal width and height) for the logo. Please ensure that the banner is at least 1920x1080 pixels.</p>
 
-                        <br><br><br><br><br><br>
+                        <br><br><br><br><br><br><br><br>
                         <div class="icon">
                           <svg  xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor"  class="bi bi-info-circle" viewBox="0 0 16 16">
                           <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
